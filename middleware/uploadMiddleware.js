3e1036ea9781689f98e7c0e8f609044c448257ee
krishna Gupta <krishnaguptasr12@gmail.com>
2026-08-17 01:58:39 +0530

    fix upload directory

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,8 +1,17 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+// uploads folder automatically create hoga
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
 
     filename: (req, file, cb) => {
