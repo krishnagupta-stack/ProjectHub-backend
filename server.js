cc81df1e5bd4f3b3f6cd275167ba830e58357a80
krishna Gupta <krishnaguptasr12@gmail.com>
2026-08-20 23:59:15 +0530

    Fix uploads static folder

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ connectDB();
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 // Uploads folder
@@ -26,6 +27,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
+// Test
 app.get("/", (req, res) => {
     res.send("ProjectHub Backend Running...");
 });
